403fb128f80f10c6639677b78fb4b66d7c82603d
Kyle McDonald <kyle@kylemcdonald.net>
2016-02-28 17:56:17 -0500

    better blob detection and added motion history

--- BlobDetection/sketch.js
+++ BlobDetection/sketch.js
@@ -1,18 +1,41 @@
 var capture;
 var tracker;
-var w = 640, h = 480;
+
+var rhi, ghi, bhi;
+var rlo, glo, blo;
+function setTarget(r, g, b, range) {
+  range = range || 32;
+  rhi = r + range, rlo = r - range;
+  ghi = g + range, glo = g - range;
+  bhi = b + range, blo = b - range;
+}
 
 function setup() {
+  var w = 640, h = 480;
   capture = createCapture(VIDEO);
-  createCanvas(w, h);
   capture.size(w, h);
-  // capture.hide();
+  capture.parent('container');
+  cnv = createCanvas(w, h);
+  cnv.parent('container');
+  // capture.hide(); // tracking.js can't track the video when it's hidden
   
+  setTarget(255, 255, 255); // by default track white
+  tracking.ColorTracker.registerColor('match', function(r, g, b) {
+    if(r <= rhi && r >= rlo &&
+       g <= ghi && g >= glo &&
+       b <= bhi && b >= blo) {
+      return true;
+    }
+    return false;
+  });
+  tracker = new tracking.ColorTracker(['match']);
+  tracker.minDimension = 20; // make this smaller to track smaller objects
   capture.elt.id = 'p5video';
-  tracker = new tracking.ColorTracker(['yellow']);
   tracking.track('#p5video', tracker, { camera: true });
   tracker.on('track', function(event) {
-    stroke(0);
+    clear();
+    strokeWeight(4);
+    stroke(255, 0, 0);
     noFill();
     event.data.forEach(function(r) {
       rect(r.x, r.y, r.width, r.height);
@@ -21,8 +44,11 @@ function setup() {
 }
 
 function draw() {
-  // image(capture, 0, 0, w, h);
-  capture.loadPixels();
-  if(capture.pixels.length > 0) { // don't forget this!
+  if(mouseIsPressed &&
+    mouseX > 0 && mouseX < width &&
+    mouseY > 0 && mouseY < height) {
+    capture.loadPixels();
+    target = capture.get(mouseX, mouseY);
+    setTarget(target[0], target[1], target[2]);
   }
 }
